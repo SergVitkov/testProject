58887b1b1674431627afebbf7a9d2350f7c64a60
Serg <sonikstar1993@gmail.com>
2024-08-28 11:37:49 +0200

    add new file

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+const vehicleBodyWidth  = 5000,
+      vehicleBodyLength = 4000;
+
+console.log("Ширина кузова автомобиля:" + vehicleBodyWidth + ',длина:' + vehicleBodyLength);
+
+// Request
+// data
+// response
+
+
+// snake_case - их никогда нельзя переопределять, так обозначают переменные которые хоятт видеть в виде констант
+// UPPER_SNAKE_CASE - их никогда нельзя переопределять, так обозначают переменные которые хоятт видеть в виде констант
+// Kebab-cace
+// PascalCase
+
+let number = 4.6;
+
+// числа 
+console.log(4/0); - получим значение Infinity
+console.log('string'* 9); - получим ответ Nan (не число)
+
+// строка
+const persone = "Alex";   
+const _persone = '5'; - /*не смотря на то что это чисто это тоже строка потому что обернута в кавычки*/
+const bool = false;
+
+/* (null and undefined) */
+console.log(something);
+
+const arr = [1, 2, 3]; - это наз. Массив
+console.log(arr[1]); - /*это обращение ко второму елементу так как счет начинаеться с 0 а не с 1*/ 
+
+
+/*const obj = {a: 1, b: 2}; - это Обьект*/
+
+const obj = {
+    'Anna': 500,
+    'Alice': 800
+};
+
+const arr = ['a','b','c'];
+const arrObj = {
+    0: 'a',
+    1: 'b',
+    2: 'c'
+};
---
+++ js/script1.js
@@ -0,0 +1,40 @@
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = prompt('Вам уже есть 18?', '');
+//console.log(typeof(answer));
+
+//const answers = [];
+
+//answers[0] = prompt("Как вас зовут?" , "");
+//answers[1] = prompt("Сколько вам лет?" , "");
+//answers[2] = prompt("Как дела?" , "");
+
+//document.write(answers);
+//console.log(answers);
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+
+//=========================//=========================//=========================
+//const user = "Serg";
+
+//alert(`Hallo, ${user}`);
+//=========================//=========================//=========================
+
+//et incr = 10,
+    //decr = 10;
+
+//incr++; //увеличение значения на единичку
+//decr--; //уменшение значения на единичку
+
+//console.log(incr);
+//console.log(decr);
+//=========================//=========================//=========================
+
+&& означает как 'и'
+|| означает как 'или'
+! оператор отрицания 
+!= переводиться как Не равен
