1f8c36409a946f20e31ef4eda5eeb2edf868bc5b
Serg <sonikstar1993@gmail.com>
2024-09-01 15:51:06 +0200

    first

--- js/script1.js
+++ js/script1.js
@@ -34,7 +34,29 @@
 //console.log(decr);
 //=========================//=========================//=========================
 
-&& означает как 'и'
+/*&& означает как 'и'
 || означает как 'или'
 ! оператор отрицания 
 != переводиться как Не равен
+
+prompt - команда Задать вопрос пользователю*/
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?,' '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотреных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотреных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
